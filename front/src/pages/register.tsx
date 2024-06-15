1c9f334a6616865ece989c9e3f89d1f9cbb1ba4c
Ty HA <bichty.ha@gmail.com>
2024-06-16 01:19:45 +0200

    register

--- front/src/pages/register.tsx
+++ front/src/pages/register.tsx
@@ -8,12 +8,12 @@ const ABI = contractData.abi;
 
 interface Ethereum {
   isMetaMask: boolean;
-  request: (request: { method: string; params?: string[] }) => Promise<string>;
+  request: (request: { method: string; params?: string[] }) => Promise<any>;
 }
 
 declare global {
   interface Window {
-    ethereum: Ethereum;
+    ethereum?: Ethereum;
   }
 }
 
@@ -41,15 +41,19 @@ interface Artist {
 }
 
 export default function RegisterArtist() {
+  const [state, setState] = React.useState<{ isArtist: boolean } | null>(null);
+
   const [signer, setSigner] = useState<ethers.JsonRpcSigner | null>(null);
   const [isArtist, setIsArtist] = useState<boolean>(false);
   const [artistAddress, setArtistAddress] = useState<string | null>(null);
   const [mainName, setMainName] = useState<string | null>(null);
   const [mainType, setMainType] = useState<ArtistType | null>(null);
   const [extraTypes, setExtraTypes] = useState<ArtistType[]>([]);
+  const [genres, setGenres] = useState<string[]>([]);
   const [assets, setAssets] = useState<string[]>([]);
-  const [submittedArtistData, setSubmittedArtistData] = useState(null);
-
+  const [submittedArtistData, setSubmittedArtistData] = useState<Artist | null>(
+    null,
+  );
   const [artistRegistered, setArtistRegistered] = useState<boolean>(false);
 
   const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -63,6 +67,7 @@ export default function RegisterArtist() {
     if (data) {
       const parsedData = JSON.parse(data);
       const artistAddress = parsedData.address;
+      console.log(artistAddress);
       setArtistAddress(artistAddress);
     } else {
       console.log('No address found');
@@ -82,47 +87,56 @@ export default function RegisterArtist() {
     getAddress();
   }, []);
 
-  const contractAddress = '0xeCd63E3c7128020C4Cb10D5d393A6cBDba1384Ec';
+  const contractAddress = '0xDfE175b1C9fcE91978B4c018442f96e94B2dBF66';
 
   const registerArtist = useCallback(async () => {
+    console.log('Register button clicked');
     if (
       isArtist &&
       contractAddress &&
       signer &&
       artistAddress &&
       mainName &&
-      mainType != null && // Ensure mainType is not null
+      mainType !== null && // Ensure mainType is not null
       extraTypes.length > 0 && // Ensure extraTypes is not empty
       assets.length > 0 // Ensure assets is not empty
     ) {
       const contract = new ethers.Contract(contractAddress, ABI, signer);
+      console.log(contractAddress);
 
       // Correctly encode the data for blockchain
-      const genresAsBytes = genres.map(ethers.utils.toUtf8Bytes);
-      const assetsAsBytes32 = assets.map(ethers.utils.formatBytes32String);
+      const genresAsBytes = genres.map(ethers.encodeBytes32String);
+      console.log(genresAsBytes);
+      const assetsAsBytes32 = assets.map(ethers.encodeBytes32String);
+      console.log(assetsAsBytes32);
 
       // Correct the function call to match the ABI
-      await contract.registerArtists(
-        isArtist,
-        mainName,
-        mainType,
-        extraTypes,
-        genresAsBytes,
-        assetsAsBytes32,
-      );
+      try {
+        const tx = await contract.registerArtists(
+          isArtist,
+          mainName,
+          mainType,
+          extraTypes.map((type) => ArtistType[type]), // Convert enum to uint8
+          genresAsBytes,
+          assetsAsBytes32,
+        );
+        await tx.wait();
+        setState({ isArtist: true });
 
-      // Update state with submitted data
-      setSubmittedArtistData({
-        isArtist,
-        artistAddress,
-        mainName,
-        mainType,
-        extraTypes,
-        genres,
-        assets,
-      });
+        setSubmittedArtistData({
+          isArtist,
+          artistAddress,
+          mainName,
+          mainType,
+          extraTypes,
+          genres,
+          assets,
+        });
 
-      setArtistRegistered(true);
+        setArtistRegistered(true);
+      } catch (error) {
+        console.error('Error registering artist:', error);
+      }
     }
   }, [
     isArtist,
@@ -140,6 +154,25 @@ export default function RegisterArtist() {
     registerArtist();
   }, [registerArtist]);
 
+  // Listen for the ArtistRegistered event
+  useEffect(() => {
+    if (signer && artistRegistered) {
+      const contract = new ethers.Contract(contractAddress, ABI, signer);
+      contract.on('ArtistRegistered', (artist, mainName, registeredAt) => {
+        console.log(
+          `Artist Registered: ${mainName} at ${new Date(registeredAt * 1000).toLocaleString()}`,
+        );
+        // Ici, vous pouvez ajouter une logique pour vérifier que l'artiste enregistré correspond à celui attendu
+        // Par exemple, comparer `mainName` avec `submittedArtistData.mainName`
+        if (submittedArtistData && mainName === submittedArtistData.mainName) {
+          console.log(
+            "L'enregistrement de l'artiste a été vérifié avec succès sur la blockchain.",
+          );
+        }
+      });
+    }
+  }, [signer, artistRegistered, submittedArtistData]);
+
   return (
     <>
       <Sidebar />
@@ -150,6 +183,20 @@ export default function RegisterArtist() {
           </h1>
 
           <div className="flex flex-col gap-2 px-12">
+            <div className="block mt-4">
+              <Label
+                className="font-bold text-white"
+                htmlFor="artistAddress"
+                value="My address"
+              />
+              <TextInput
+                id="artistAddress"
+                placeholder={artistAddress || 'No address found'}
+                required
+                value="0x8f1951BE5C9A79d7AFdE6F931d28E7C632bC6C9D"
+                onChange={(e) => setArtistAddress(e.target.value)}
+              />
+            </div>
             <div className="block mt-4">
               <label className="font-bold text-white mr-4" htmlFor="isArtist">
                 Are you an artist?
@@ -185,20 +232,23 @@ export default function RegisterArtist() {
             </div>
             <Select
               id="mainType"
-              value={mainType || ''}
+              value={mainType !== null ? mainType : ''}
               required
-              onChange={(e) => setMainType(e.target.value)}
+              onChange={(e) => setMainType(parseInt(e.target.value))}
+              style={{ color: 'black' }}
             >
-              <option>Singer</option>
-              <option>Instrumentalist</option>
-              <option>Composer</option>
-              <option>Lyricist</option>
-              <option>Producer</option>
-              <option>DiscJokey</option>
-              <option>Conductor</option>
-              <option>Arranger</option>
-              <option>Engineer</option>
-              <option>Director</option>
+              <option value={ArtistType.Singer}>Singer</option>
+              <option value={ArtistType.Instrumentalist}>
+                Instrumentalist
+              </option>
+              <option value={ArtistType.Composer}>Composer</option>
+              <option value={ArtistType.Lyricist}>Lyricist</option>
+              <option value={ArtistType.Producer}>Producer</option>
+              <option value={ArtistType.DiscJokey}>DiscJokey</option>
+              <option value={ArtistType.Conductor}>Conductor</option>
+              <option value={ArtistType.Arranger}>Arranger</option>
+              <option value={ArtistType.Engineer}>Engineer</option>
+              <option value={ArtistType.Director}>Director</option>
             </Select>
             <div className="block mt-4">
               <Label
@@ -223,10 +273,12 @@ export default function RegisterArtist() {
                       onChange={(e) => {
                         const newExtraTypes = [...extraTypes];
                         if (e.target.checked) {
-                          newExtraTypes.push(ArtistType[type]);
+                          newExtraTypes.push(
+                            ArtistType[type as keyof typeof ArtistType],
+                          );
                         } else {
                           const indexToRemove = newExtraTypes.indexOf(
-                            ArtistType[type],
+                            ArtistType[type as keyof typeof ArtistType],
                           );
                           if (indexToRemove > -1) {
                             newExtraTypes.splice(indexToRemove, 1);
@@ -240,6 +292,24 @@ export default function RegisterArtist() {
                     </label>
                   </div>
                 ))}
+            </div>
+            <div className="block">
+              <Label
+                className="font-bold text-white"
+                htmlFor="genres"
+                value="Genres (comma-separated)"
+              />
+              <TextInput
+                id="genres"
+                placeholder="Enter genres, separated by commas"
+                required
+                value={genres.join(', ')}
+                onChange={(e) =>
+                  setGenres(
+                    e.target.value.split(',').map((genre) => genre.trim()),
+                  )
+                }
+              />
             </div>
             <div className="block">
               <Label
@@ -270,13 +340,29 @@ export default function RegisterArtist() {
           </Button>
         </section>
         <section>
-          {submittedArtistData && (
-            <div className="submitted-data">
-              <h2>Artist Data Submitted</h2>
-              <p>Main Name: {submittedArtistData.mainName}</p>
-              <p>Main Type: {submittedArtistData.mainType}</p>
-              {/* Afficher d'autres données de l'artiste ici */}
-            </div>
+          {artistRegistered && submittedArtistData && (
+            <section>
+              <h2>Artist Registration Successful</h2>
+              <p>
+                <strong>Name:</strong> {submittedArtistData.mainName}
+              </p>
+              <p>
+                <strong>Type:</strong>{' '}
+                {ArtistType[submittedArtistData.mainType]}
+              </p>
+              <p>
+                <strong>Extra Types:</strong>{' '}
+                {submittedArtistData.extraTypes
+                  .map((type) => ArtistType[type])
+                  .join(', ')}
+              </p>
+              <p>
+                <strong>Genres:</strong> {submittedArtistData.genres.join(', ')}
+              </p>
+              <p>
+                <strong>Assets:</strong> {submittedArtistData.assets.join(', ')}
+              </p>
+            </section>
           )}
         </section>
       </main>
